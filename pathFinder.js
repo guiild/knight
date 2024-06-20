f310fe2792c460a41c70634a72aa27ba9a00a0d9
rachOS <g.chamekh@e-nautia.com>
2024-06-20 22:12:39 +0200

    Add gridSquare property and related tests to PathFinder
    A new property, 'gridSquare', has been added to the PathFinder class, and a new constructor initializes this property. Corresponding tests have been added to make sure the gridSquare is well defined, an array, and has at least 64 cells (default 8x8) when initialized.

--- pathFinder.js
+++ pathFinder.js
@@ -1,6 +1,15 @@
 class PathFinder {
+    gridSquare= []
+
+    constructor() {
+        this.createGridSquare()
+    }
     getMinimumMoves = (position, target) => {
         return 0
+    }
+
+    createGridSquare = (sideSize = 8) => {
+        this.gridSquare = Array.from(Array(sideSize**2).keys())
     }
 }
 
--- pathFinder.test.js
+++ pathFinder.test.js
@@ -1,4 +1,5 @@
 const PathFinder = require('./pathFinder');
+const finder = require("./pathFinder");
 
 describe('Pathfinder', () => {
     let finder;
@@ -7,7 +8,26 @@ describe('Pathfinder', () => {
         finder = new PathFinder();
     })
 
-    describe('use cases', () => {
+    describe('TDD', () => {
+        test('should have a grid defined', () => {
+            expect(finder).toHaveProperty("gridSquare");
+        });
+
+        test('should have an gridSquare', () => {
+            const gridSquare = finder.gridSquare;
+
+            expect(Array.isArray(gridSquare)).toBeTruthy();
+        });
+
+        test('should have an with at least a 8x8 cells', () => {
+            finder.createGridSquare(10)
+            const gridSquare = finder.gridSquare;
+
+            expect(gridSquare.length).toBeGreaterThanOrEqual(64);
+        });
+    })
+
+    describe.skip('use cases', () => {
         test('From 1 to 1', () => {
             expect(finder.getMinimumMoves(1, 1)).toBe(0);
         });
